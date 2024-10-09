98860518f7bc4a9bb02471aaee95f00aacfe1589
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2024-10-10 07:44:37 +0800

    lib.lua

--- lib.lua
+++ lib.lua
@@ -184,21 +184,6 @@ function setvalue(address,flags,value) PS('Modify address value (address, numeri
  function lucha(Search, Write, Type) gg.clearResults() gg.setVisible(false) gg.searchNumber(Search[1][1], Type) local count = gg.getResultCount() local result = gg.getResults(count) gg.clearResults() local data = {} local base = Search[1][2] if (count > 0) then for i, v in ipairs(result) do v.isUseful = true end for k=2, #Search do local tmp = {} local offset = Search[k][2] - base local num = Search[k][1] for i, v in ipairs(result) do tmp[#tmp+1] = {} tmp[#tmp].address = v.address + offset tmp[#tmp].flags = v.flags end tmp = gg.getValues(tmp) for i, v in ipairs(tmp) do if ( tostring(v.value) ~= tostring(num) ) then result[i].isUseful = false end end end for i, v in ipairs(result) do if (v.isUseful) then data[#data+1] = v.address end end if (#data > 0) then gg.toast('Results '..#data..' Edited') local t = {} local base = Search[1][2] for i=1, #data do for k, w in ipairs(Write) do offset = w[2] - base t[#t+1] = {} t[#t].address = data[i] + offset t[#t].flags = Type t[#t].value = w[1] if (w[3] == true) then local item = {} item[#item+1] = t[#t] item[#item].freeze = true gg.addListItems(item) end end end gg.setValues(t) else return false end else return false end end
  
 
-
-function A2()
-p = gg.choice({
-" 𝗟𝗢𝗕𝗕𝗬 ",
-" 𝗜𝗡𝗚𝗔𝗠𝗘 ",
-" Back ",
-},nil,"💠 KYUMA 𝗩𝗜𝗣 𝗦𝗖𝗥𝗜𝗣𝗧 💠")
-if p == nil then else
-if p == 1 then f1() end
-if p == 2 then f2() end
-if p == 3 then HOME() end
-end
-ces = -1
-end
-
 function f1()
 e = gg.multiChoice({
 " Aimbot All Guns " .. z,
